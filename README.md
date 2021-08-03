639f5c6479c874584bdf6e16a80ace23d44cd248
Ryan Robinson <ryan.robinson1@caci.com>
2021-08-03 14:30:34 -0400

    Added insecure flag to bipass tls x509 errors

--- README.md
+++ README.md
@@ -10,12 +10,17 @@ getCertExpiry is a go command line tool to find the expiration dates of given se
 ## Usage
 
 
- <font size="3">TODO: Description of command</font> <pre>$ ./getCertExpiry <span style="color:magenta"><i><b>ADDRESS</b></i></span>:<span style="color:magenta"><i><b>PORT</b></i></span> </pre>
-
-
+ <font size="3">Gets the expiration date of the specified server cert. Use the ``--insecure`` flag to allow insecure TLS connections</font> <pre>$ ./getCertExpiry <span style="color:magenta"><i><b>ADDRESS</b></i></span>:<span style="color:magenta"><i><b>PORT</b></i></span> [--insecure] </pre>
+## Exit Codes
+* _Exit Code 0 : Cert is valid_
+* _Exit Code 1 : Cert is expired_
+* _Exit Code 3 : Certs are not supported_
+* _Exit Code 4 : Cert is untrusted_
+* _Exit Code 5 : Invalid Args_
 
 ## Example
     $ ./getCertExpiry example.com:5000
+    $ ./getCertExpiry example.com:5000 --insecure
 
 
  
--- getCertExpiry.go
+++ getCertExpiry.go
@@ -2,6 +2,8 @@
 	Ryan Robinson, 2021
 
 	getCertExpiry is a go command line tool to find the expiration dates of given server certs and check if they are expired
+
+	TODO: Fix error message when mixed flag arguments are entered
 */
 
 package main
@@ -9,6 +11,7 @@ package main
 import (
 	"crypto/tls"
 	"errors"
+	"flag"
 	"fmt"
 	"os"
 	"time"
@@ -21,8 +24,11 @@ func isTimePast(t time.Time) bool {
 }
 
 //Connects to address and returns 0 if the cert is valid and 1 if it is expired in addition to the cert expiration date. If the server does not support SSL certificates, return 3 and an error.
-func getCertExpiry(address string) (int, string, error) {
-	conn, err := tls.Dial("tcp", address, nil)
+func getCertExpiry(address string, skipVerify bool) (int, string, error) {
+	conf := &tls.Config{
+		InsecureSkipVerify: skipVerify,
+	}
+	conn, err := tls.Dial("tcp", address, conf)
 	if err != nil {
 		return 3, "", err
 	}
@@ -33,23 +39,34 @@ func getCertExpiry(address string) (int, string, error) {
 	return 0, expiry.Format("2006-01-02 15:04:05"), nil
 }
 
-func parseArgs(args []string) (string, error) {
-	if len(args) != 2 {
-		return "", errors.New("args error: args formatted incorrectly, refer to README for correct formatting")
+//Parses the first argument for the address and then looks for flags. Currently the only flag is the "insecure" flag which allows for insecure tls connections
+func parseArgs(args []string) (string, bool, error) {
+	if len(args) < 2 {
+		return "", false, errors.New("args error: no args, refer to README for arg format")
 	}
-	return args[1], nil
+
+	subAdr := flag.NewFlagSet("adr", flag.ExitOnError)
+	secureFlag := subAdr.Bool("insecure", false, "Allow insecure tls connections")
+	subAdr.Parse(os.Args[2:])
+
+	return args[1], *secureFlag, nil
 }
 
-//Exits with status 0 if cert valid and supported, 1 if expired, 3 if not supported, 4 if untrusted (TODO), and 5 if the args are incorrectly formatted
+//Exits with status 0 if cert valid and supported, 1 if expired, 3 if not supported, 4 if untrusted (TODO), and 5 if there are no args
 func main() {
-	args, err := parseArgs(os.Args)
+	args, secure, err := parseArgs(os.Args)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(5)
 	}
 
-	status, expiry, err := getCertExpiry(args)
+	status, expiry, err := getCertExpiry(args, secure)
+
 	if err != nil {
+		if err.Error() == "x509: certificate signed by unknown authority" && !secure {
+			fmt.Println(err.Error())
+			os.Exit(4)
+		}
 		fmt.Printf("Server doesn't support SSL certificate err: " + err.Error())
 	}
 	fmt.Println(expiry)
