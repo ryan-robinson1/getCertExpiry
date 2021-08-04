8070e349543e0a720da00ae448f497508d4bd24a
Ryan Robinson <ryan.robinson1@caci.com>
2021-08-04 14:54:30 -0400

    tls certificate handling ca

--- getCertExpiry.go
+++ getCertExpiry.go
@@ -46,15 +46,29 @@ func isTimePast(t time.Time) bool {
 }
 
 //Connects to address and returns 0 if the cert is valid and 1 if it is expired in addition to the cert expiration date. If the server does not support SSL certificates, return 3 and an error.
-func getCertExpiry(address string, skipVerify bool, cert tls.Certificate) (int, string, error) {
+func getCertExpiry(address string, skipVerify bool) (int, string, error) {
+	cert, err := tls.LoadX509KeyPair("ryan.crt", "ryan.key")
+	if err != nil {
+		log.Fatal(err)
+	}
+	caCert, err := ioutil.ReadFile("ca.crt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
 	conf := &tls.Config{
 		Certificates:       []tls.Certificate{cert},
+		RootCAs:            caCertPool,
 		InsecureSkipVerify: skipVerify,
 	}
+	conf.BuildNameToCertificate()
 	conn, err := tls.Dial("tcp", address, conf)
 	if err != nil {
 		return 3, "", err
 	}
+
 	expiry := conn.ConnectionState().PeerCertificates[0].NotAfter
 	if isTimePast(expiry) {
 		return 1, expiry.Format("2006-01-02 15:04:05"), nil
@@ -63,35 +77,27 @@ func getCertExpiry(address string, skipVerify bool, cert tls.Certificate) (int, 
 }
 
 //Parses the first argument for the address and then looks for flags. Currently the only flag is the "insecure" flag which allows for insecure tls connections
-func parseArgs(args []string) (string, bool, *tls.Certificate, error) {
+func parseArgs(args []string) (string, bool, error) {
 	if len(args) < 2 {
-		return "", false, nil, errors.New("args error: no args, refer to README for arg format")
+		return "", false, errors.New("args error: no args, refer to README for arg format")
 	}
 
 	subAdr := flag.NewFlagSet("adr", flag.ExitOnError)
-
 	secureFlag := subAdr.Bool("insecure", false, "Allow insecure tls connections")
-	certFile := subAdr.String("cert", "ryan.crt", "A PEM eoncoded certificate file.")
-	keyFile := subAdr.String("key", "ryan.key", "A PEM encoded private key file.")
-
-	cert, err := tls.LoadX509KeyPair(*certFile, *keyFile)
-	if err != nil {
-		log.Fatal(err)
-	}
 	subAdr.Parse(os.Args[2:])
 
-	return args[1], *secureFlag, &cert, nil
+	return args[1], *secureFlag, nil
 }
 
 //Exits with status 0 if cert valid and supported, 1 if expired, 3 if not supported, 4 if untrusted (TODO), and 5 if there are no args
 func main() {
-	args, secure, tlscert, err := parseArgs(os.Args)
+	args, secure, err := parseArgs(os.Args)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(5)
 	}
 
-	status, expiry, err := getCertExpiry(args, secure, *tlscert)
+	status, expiry, err := getCertExpiry(args, secure)
 
 	if err != nil {
 		if err.Error() == "x509: certificate signed by unknown authority" && !secure {
